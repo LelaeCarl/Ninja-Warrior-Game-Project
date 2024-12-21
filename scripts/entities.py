9299dfdd6877b863955935607aef93cb99ffebcc
Sage <carllelae@gmail.com>
2024-12-21 16:03:29 +0800

    Added ability to jump twice

--- scripts/entities.py
+++ scripts/entities.py
@@ -150,7 +150,7 @@ class Player(PhysicsEntity):
     def __init__(self, game, pos, size):
         super().__init__(game, 'player', pos, size)
         self.air_time = 0
-        self.jumps = 1
+        self.jumps = 2
         self.wall_slide = False
         self.dashing = 0
 
